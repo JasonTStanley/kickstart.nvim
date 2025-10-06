a28eeff2b7d187c7cb8d932d4dacca596007af08
Jason Stanley <jtstanle@ucsd.edu>
2025-10-06 09:56:36 -0700

    adding terminal setup things

--- init.lua
+++ init.lua
@@ -385,6 +385,18 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>la', ':Telescope ros2 actions<CR>', { desc = '[ROS 2]: List actions' })
       vim.keymap.set('n', '<leader>lt', ':Telescope ros2 topics<CR>', { desc = '[ROS 2]: List topics' })
       vim.keymap.set('n', '<leader>ls', ':Telescope ros2 services<CR>', { desc = '[ROS 2]: List services' })
+      vim.api.nvim_command [[
+  command! ColconBuild :! CC=clang CXX=clang++ colcon build --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+]]
+      vim.api.nvim_command [[
+  command! -nargs=1 ColconBuildSingle :! CC=clang CXX=clang++ colcon build --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON --packages-up-to <args>
+]]
+      vim.api.nvim_command [[
+  command! ColconBuildDebug :! CC=clang CXX=clang++ colcon build --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug
+]]
+      vim.api.nvim_command [[
+  command! -nargs=1 ColconBuildDebugSingle :! CC=clang CXX=clang++ colcon build --symlink-install --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug --packages-up-to <args>
+]]
     end,
   },
   -- NOTE: Plugins can specify dependencies.
