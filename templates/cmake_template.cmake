ebae0ea4e6df92a75ad01f1df87b0c7a4f22b6c9
Jason Stanley <jtstanle@ucsd.edu>
2026-01-06 11:37:27 -0800

    format

--- templates/cmake_template.cmake
+++ templates/cmake_template.cmake
@@ -1,5 +1,8 @@
 cmake_minimum_required(VERSION 3.12)
-project(ProjectName VERSION 0.1.0 LANGUAGES CXX)
+project(
+  ProjectName
+  VERSION 0.1.0
+  LANGUAGES CXX)
 
 # name of executable, change as desired
 set(PROJECT_EXEC "${PROJECT_NAME}_exec")
@@ -8,7 +11,9 @@ set(PROJECT_EXEC "${PROJECT_NAME}_exec")
 # Default to Release build if user did not specify
 # ------------------------------------------------------------
 if(NOT CMAKE_BUILD_TYPE)
-    set(CMAKE_BUILD_TYPE "Release" CACHE STRING "Build type" FORCE)
+  set(CMAKE_BUILD_TYPE
+      "Release"
+      CACHE STRING "Build type" FORCE)
 endif()
 
 # ------------------------------------------------------------
@@ -17,28 +22,26 @@ endif()
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON) #allows for tools like clang-tidy to work
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON) # allows for tools like clang-tidy to work
 
 # ------------------------------------------------------------
 # (Optional) Output directories
 # ------------------------------------------------------------
-#set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
-#set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
-#set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
+# set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+# set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
+# set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
 
 # ------------------------------------------------------------
 # Include paths
 # ------------------------------------------------------------
-# Automatically collect all cpp files from src/
-# Add your include directory (`include/`)
+# Automatically collect all cpp files from src/ Add your include directory
+# (`include/`)
 file(GLOB_RECURSE PROJECT_SOURCES ${PROJECT_SOURCE_DIR}/src/*.cpp)
 
 add_executable(${PROJECT_EXEC} ${PROJECT_SOURCES})
 
 target_include_directories(${PROJECT_EXEC}
-    PRIVATE
-        ${PROJECT_SOURCE_DIR}/include
-)
+                           PRIVATE ${PROJECT_SOURCE_DIR}/include)
 
 # ------------------------------------------------------------
 # Dependencies
@@ -46,38 +49,28 @@ target_include_directories(${PROJECT_EXEC}
 # ===== SYSTEM DEPENDENCIES =====
 # Find and link system-wide packages here:
 #
-#   find_package(Eigen3 REQUIRED)
-#   find_package(OpenCV REQUIRED)
+# find_package(Eigen3 REQUIRED) find_package(OpenCV REQUIRED)
 #
-#   target_include_directories(${PROJECT_EXEC} PRIVATE ${OpenCV_INCLUDE_DIRS})
-#   target_link_libraries(${PROJECT_EXEC} PRIVATE Eigen3::Eigen ${OpenCV_LIBRARIES})
+# target_include_directories(${PROJECT_EXEC} PRIVATE ${OpenCV_INCLUDE_DIRS})
+# target_link_libraries(${PROJECT_EXEC} PRIVATE Eigen3::Eigen
+# ${OpenCV_LIBRARIES})
 #
 
 # ===== LOCAL / THIRD-PARTY DEPENDENCIES =====
 # If you include local libraries as subdirectories:
 #
-#   add_subdirectory(external/mylib)
-#   target_link_libraries(${PROJECT_EXEC} PRIVATE mylib)
+# add_subdirectory(external/mylib) target_link_libraries(${PROJECT_EXEC} PRIVATE
+# mylib)
 #
 
 # ===== OPTIONAL DEPENDENCIES =====
 # Add optional dependencies behind options:
 #
-#   option(USE_FAST_MATH "Enable fast math" OFF)
-#   if(USE_FAST_MATH)
-#       target_compile_definitions(${PROJECT_EXEC} PRIVATE ENABLE_FAST_MATH)
-#   endif()
+# option(USE_FAST_MATH "Enable fast math" OFF) if(USE_FAST_MATH)
+# target_compile_definitions(${PROJECT_EXEC} PRIVATE ENABLE_FAST_MATH) endif()
 #
 
 # ------------------------------------------------------------
 # Additional compile flags (optional)
 # ------------------------------------------------------------
-target_compile_options(${PROJECT_EXEC} PRIVATE
-    -Wall
-    -Wextra
-    -Wpedantic
-)
-
-
-
-
+target_compile_options(${PROJECT_EXEC} PRIVATE -Wall -Wextra -Wpedantic)
